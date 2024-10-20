c3a3dc42761bd602dd9f1d6ce28222c8fad13809
heetjain09 <heet.236057101@vcet.edu.in>
2024-10-20 01:30:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,238 +1,120 @@
-let country_list = {
-  AED: "AE",
-  AFN: "AF",
-  XCD: "AG",
-  ALL: "AL",
-  AMD: "AM",
-  ANG: "AN",
-  AOA: "AO",
-  AQD: "AQ",
-  ARS: "AR",
-  AUD: "AU",
-  AZN: "AZ",
-  BAM: "BA",
-  BBD: "BB",
-  BDT: "BD",
-  XOF: "BE",
-  BGN: "BG",
-  BHD: "BH",
-  BIF: "BI",
-  BMD: "BM",
-  BND: "BN",
-  BOB: "BO",
-  BRL: "BR",
-  BSD: "BS",
-  NOK: "BV",
-  BWP: "BW",
-  BYR: "BY",
-  BZD: "BZ",
-  CAD: "CA",
-  CDF: "CD",
-  XAF: "CF",
-  CHF: "CH",
-  CLP: "CL",
-  CNY: "CN",
-  COP: "CO",
-  CRC: "CR",
-  CUP: "CU",
-  CVE: "CV",
-  CYP: "CY",
-  CZK: "CZ",
-  DJF: "DJ",
-  DKK: "DK",
-  DOP: "DO",
-  DZD: "DZ",
-  ECS: "EC",
-  EEK: "EE",
-  EGP: "EG",
-  ETB: "ET",
-  EUR: "FR",
-  FJD: "FJ",
-  FKP: "FK",
-  GBP: "GB",
-  GEL: "GE",
-  GGP: "GG",
-  GHS: "GH",
-  GIP: "GI",
-  GMD: "GM",
-  GNF: "GN",
-  GTQ: "GT",
-  GYD: "GY",
-  HKD: "HK",
-  HNL: "HN",
-  HRK: "HR",
-  HTG: "HT",
-  HUF: "HU",
-  IDR: "ID",
-  ILS: "IL",
-  INR: "IN",
-  IQD: "IQ",
-  IRR: "IR",
-  ISK: "IS",
-  JMD: "JM",
-  JOD: "JO",
-  JPY: "JP",
-  KES: "KE",
-  KGS: "KG",
-  KHR: "KH",
-  KMF: "KM",
-  KPW: "KP",
-  KRW: "KR",
-  KWD: "KW",
-  KYD: "KY",
-  KZT: "KZ",
-  LAK: "LA",
-  LBP: "LB",
-  LKR: "LK",
-  LRD: "LR",
-  LSL: "LS",
-  LTL: "LT",
-  LVL: "LV",
-  LYD: "LY",
-  MAD: "MA",
-  MDL: "MD",
-  MGA: "MG",
-  MKD: "MK",
-  MMK: "MM",
-  MNT: "MN",
-  MOP: "MO",
-  MRO: "MR",
-  MTL: "MT",
-  MUR: "MU",
-  MVR: "MV",
-  MWK: "MW",
-  MXN: "MX",
-  MYR: "MY",
-  MZN: "MZ",
-  NAD: "NA",
-  XPF: "NC",
-  NGN: "NG",
-  NIO: "NI",
-  NPR: "NP",
-  NZD: "NZ",
-  OMR: "OM",
-  PAB: "PA",
-  PEN: "PE",
-  PGK: "PG",
-  PHP: "PH",
-  PKR: "PK",
-  PLN: "PL",
-  PYG: "PY",
-  QAR: "QA",
-  RON: "RO",
-  RSD: "RS",
-  RUB: "RU",
-  RWF: "RW",
-  SAR: "SA",
-  SBD: "SB",
-  SCR: "SC",
-  SDG: "SD",
-  SEK: "SE",
-  SGD: "SG",
-  SKK: "SK",
-  SLL: "SL",
-  SOS: "SO",
-  SRD: "SR",
-  STD: "ST",
-  SVC: "SV",
-  SYP: "SY",
-  SZL: "SZ",
-  THB: "TH",
-  TJS: "TJ",
-  TMT: "TM",
-  TND: "TN",
-  TOP: "TO",
-  TRY: "TR",
-  TTD: "TT",
-  TWD: "TW",
-  TZS: "TZ",
-  UAH: "UA",
-  UGX: "UG",
-  USD: "US",
-  UYU: "UY",
-  UZS: "UZ",
-  VEF: "VE",
-  VND: "VN",
-  VUV: "VU",
-  YER: "YE",
-  ZAR: "ZA",
-  ZMK: "ZM",
-  ZWD: "ZW"
-};
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
-let apiKey = "e759f92560e41c99ee6213a2";
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
 
-const dropList = document.querySelectorAll("form select"),
-  fromCurrency = document.querySelector(".from select"),
-  toCurrency = document.querySelector(".to select"),
-  getButton = document.querySelector("form button");
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
 
-for (let i = 0; i < dropList.length; i++) {
-  for (let currency_code in country_list) {
-    let selected =
-      i == 0
-        ? currency_code == "USD"
-          ? "selected"
-          : ""
-        : currency_code == "INR"
-        ? "selected"
-        : "";
-    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-    dropList[i].insertAdjacentHTML("beforeend", optionTag);
-  }
-  dropList[i].addEventListener("change", (e) => {
-    loadFlag(e.target);
-  });
-}
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
-function loadFlag(element) {
-  for (let code in country_list) {
-    if (code == element.value) {
-      let imgTag = element.parentElement.querySelector("img");
-      imgTag.src = `https://flagcdn.com/48x36/${country_list[
-        code
-      ].toLowerCase()}.png`;
-    }
-  }
-}
-
-window.addEventListener("load", () => {
-  getExchangeRate();
-});
-
-getButton.addEventListener("click", (e) => {
+// Add transaction
+function addTransaction(e) {
   e.preventDefault();
-  getExchangeRate();
-});
 
-const exchangeIcon = document.querySelector("form .icon");
-exchangeIcon.addEventListener("click", () => {
-  let tempCode = fromCurrency.value;
-  fromCurrency.value = toCurrency.value;
-  toCurrency.value = tempCode;
-  loadFlag(fromCurrency);
-  loadFlag(toCurrency);
-  getExchangeRate();
-});
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
 
-function getExchangeRate() {
-  const amount = document.querySelector("form input");
-  const exchangeRateTxt = document.querySelector("form .exchange-rate");
-  let amountVal = amount.value;
-  if (amountVal == "" || amountVal == "0") {
-    amount.value = "1";
-    amountVal = 1;
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
   }
-  exchangeRateTxt.innerText = "Getting exchange rate...";
-  let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((result) => {
-      let exchangeRate = result.conversion_rates[toCurrency.value];
-      let totalExRate = (amountVal * exchangeRate).toFixed(2);
-      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
-    })
-    .catch(() => {
-      exchangeRateTxt.innerText = "Something went wrong";
-    });
 }
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  // Get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
